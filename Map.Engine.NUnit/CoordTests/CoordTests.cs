32a784030db98b24e92c950448591f45d2f46d22
Steven Chock <steven.chock@outlook.com>
2022-06-03 06:24:06 -0600

    Remove Obsolete AngularCoord

--- Map.Engine.NUnit/CoordTests/CoordTests.cs
+++ Map.Engine.NUnit/CoordTests/CoordTests.cs
@@ -7,25 +7,7 @@
 		/// Precision varies significantly depending on the calculation you use.
 		/// </summary>
 		[Test]
-		public void Coord_Can_Calculate_Distance_Accurately_N_W()
-		{
-			const int precision = 5;
-			var known = (float)Math.Round(314.077362f, precision);
-			var sourceAngLat = new AngularCoord(3, 7, 13);
-			var sourceAngLon = new AngularCoord(3, 0, 0);
-			var targetAngLat = new AngularCoord(5, 7, 13);
-			var targetAngLon = new AngularCoord(1, 0, 0);
-			var sourceCoord = new Coord(sourceAngLat, CardinalLat.N, sourceAngLon, CardinalLong.W);
-			var targetCoord = new Coord(targetAngLat, CardinalLat.N, targetAngLon, CardinalLong.W);
-
-			var calculatedDistance = (float)Math.Round(sourceCoord.DistanceKm(targetCoord), precision);
-			Assert.That(calculatedDistance, Is.EqualTo(known));
-		}
-		/// <summary>
-		/// Precision varies significantly depending on the calculation you use.
-		/// </summary>
-		[Test]
-		public void Coord_Can_Calculate_Distance_Accurately_N_E_N_W()
+		public void Coord_Can_Calculate_Distance_Accurately_RawCoord()
 		{
 			const int precision = 5;
 			var known = (float)Math.Round(8814.41504f, precision);
--- Map.Engine/Coord.cs
+++ Map.Engine/Coord.cs
@@ -1,62 +1,21 @@
 ﻿namespace Map.Engine
 {
-	[Obsolete("I built this thinking I could get a nice database full of angular coordinates. Looks like it'll be easier to get the double lat/long instead. Keep for now.")]
-	public struct AngularCoord
-	{
-		public AngularCoord(double degrees, double minutes, double seconds)
-		{
-			Degrees = degrees;
-			Minutes = minutes;
-			Seconds = seconds;
-		}
-
-		public AngularCoord(double coord)
-		{
-			Degrees = Math.Round(coord, 0);
-			Minutes = Math.Round((coord - Degrees) * 60f, 0);
-			Seconds = Math.Round((coord - Degrees - Minutes / 60f) * 3600f, 0);
-		}
-
-		public double Degrees { get; init; }
-		public double Minutes { get; init; }
-		public double Seconds { get; init; }
-
-		public double ToDouble()
-		{
-			return (double)(Degrees + Minutes / 60f + Seconds / 3600f);
-		}
-	}
-
 	public struct Coord
 	{
 		public const double MAX_LAT_DEG = 90f;
 		public const double MAX_LONG_DEG = 180f;
 
-		public Coord(AngularCoord latAng, CardinalLat latCard, AngularCoord lonAng, CardinalLong lonCard)
-		{
-			Latitude = latAng.ToDouble();
-			Longitude = lonAng.ToDouble();
-			LatAng = latAng;
-			LongAng = lonAng;
-			LatCardinal = latCard;
-			LongCardinal = lonCard;
-		}
-
 		public Coord(double lat, double lon)
 		{
 			Latitude = lat;
 			Longitude = lon;
-			LatAng = new AngularCoord(lat);
-			LongAng = new AngularCoord(lon);
 			LatCardinal = CalculateCardinalLat(lat);
 			LongCardinal = CalculateCardinalLong(lon);
 		}
 
 		public double Latitude { get; }
-		public AngularCoord LatAng { get; }
 		public CardinalLat LatCardinal { get; }
 		public double Longitude { get; }
-		public AngularCoord LongAng { get; init; }
 		public CardinalLong LongCardinal { get; init; }
 
 		/// <summary>
