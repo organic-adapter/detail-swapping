20b862a6c070be7ba1140d8bc943d93fc58e8798
Steven Chock <steven.chock@outlook.com>
2022-06-09 06:57:45 -0600

    Phase 1.2 Country and Settlement maps

--- WarGames.Resources/Game/SimpleMapEntry.cs
+++ WarGames.Resources/Game/SimpleMapEntry.cs
@@ -2,16 +2,16 @@
 {
 	internal class SimpleMapEntry
 	{
-		public string admin_name { get; set; }
-		public string capital { get; set; }
-		public string city { get; set; }
-		public string city_ascii { get; set; }
-		public string country { get; set; }
-		public string id { get; set; }
-		public string iso2 { get; set; }
-		public string iso3 { get; set; }
-		public string lat { get; set; }
-		public string lng { get; set; }
-		public string population { get; set; }
+		public string admin_name { get; set; } = string.Empty;
+		public string capital { get; set; } = string.Empty;
+		public string city { get; set; } = string.Empty;
+		public string city_ascii { get; set; } = string.Empty;
+		public string country { get; set; } = string.Empty;
+		public string id { get; set; } = string.Empty;
+		public string iso2 { get; set; } = string.Empty;
+		public string iso3 { get; set; } = string.Empty;
+		public string lat { get; set; } = string.Empty;
+		public string lng { get; set; } = string.Empty;
+		public string population { get; set; } = string.Empty;
 	}
 }
--- WarGames.Resources/Game/WorldMapperProfiles.cs
+++ WarGames.Resources/Game/WorldMapperProfiles.cs
@@ -7,13 +7,21 @@ namespace WarGames.Resources.Game
 {
 	public class WorldMapperProfiles : Profile
 	{
-		public WorldMapperProfiles()
+		private readonly IRepository<Country, string> countryRepository;
+
+		public WorldMapperProfiles(IRepository<Country, string> countryRepository)
 		{
+			this.countryRepository = countryRepository;
+			CreateMap<SimpleMapEntry, Country>()
+				.ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.country))
+				.ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.country))
+				.ForMember(dest => dest.TerrainType, opt => opt.MapFrom(src => TerrainType.Land));
+
 			CreateMap<SimpleMapEntry, Settlement>()
 				.ForMember(dest => dest.Id, opt => opt.MapFrom(src => Guid.NewGuid()))
 				.ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.city))
 				.ForMember(dest => dest.TargetValues, opt => opt.MapFrom(src => new List<TargetValue>() { new CivilianPopulation(int.Parse(src.population)) }))
-				.ForMember(dest => dest.Location, opt => opt.MapFrom(src => new Location(new Country() { }, new Coord(double.Parse(src.lat), double.Parse(src.lng)))));
+				.ForMember(dest => dest.Location, opt => opt.MapFrom(src => new Location(countryRepository.Get(src.country), new Coord(double.Parse(src.lat), double.Parse(src.lng)))));
 		}
 	}
 }
